7fccfa5db6ff9d1011bce0d503a26fb78e558949
Ranyar <r4n74r@gmail.com>
2020-10-27 21:03:22 +0500

    Финальные правки в декорацию главной

--- js/writeus.js
+++ js/writeus.js
@@ -2,7 +2,26 @@ const contactsLink = document.querySelector(".contacts-button");
 const contactsPopup = document.querySelector(".modal-writeus");
 const contactsClose = contactsPopup.querySelector(".modal-close");
 
-contactsClose.addEventListener("click", function (evt) {
+contactsLink.onclick = function () {
+  console.log('Клик по кнопке, отладка');
+}
+
+contactsLink.addEventListener("click", function (evt) {
   evt.preventDefault();
   contactsPopup.classList.add("modal-show");
+    console.log('Добавили класс, отладка');
+});
+
+contactsClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  contactsPopup.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (contactsPopup.classList.contains("modal-show")) {
+      evt.preventDefault();
+      contacts.classList.remove("modal-show");
+    }
+  }
 });
