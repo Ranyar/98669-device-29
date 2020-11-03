cb3e25227c26fc6705b6f60ef19b3bc1e1f5886e
Ranyar <r4n74r@gmail.com>
2020-11-04 00:01:14 +0500

    Правки в доступную верстку, часть 2

---
+++ js/slider.js
@@ -0,0 +1,12 @@
+const slide1 = document.querySelector(".slide-1");
+const slide2 = document.querySelector(".slide-1");
+const slide3 = document.querySelector(".slide-1");
+
+const button1 = document.querySelector(".slide-1");
+const button2 = document.querySelector(".slide-1");
+const button3 = document.querySelector(".slide-1");
+
+button2.onclick = function () {
+  slide1.classList.toggle("slide-hidden");
+  // я ведь в правильную сторону думаю, правда?^^
+}
