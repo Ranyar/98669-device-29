0aad091b302d5150bc55f494038bce3b46ef156d
Ranyar <r4n74r@gmail.com>
2020-11-04 22:33:59 +0500

    Финальные правки

--- js/writeus.js
+++ js/writeus.js
@@ -1,29 +1,57 @@
 const contactsLink = document.querySelector(".contacts-button");
 const contactsPopup = document.querySelector(".modal-writeus");
-const contactsName = contactsPopup.querySelector(".contacts-name");
 const contactsClose = contactsPopup.querySelector(".modal-close");
+const contactsForm = contactsPopup.querySelector(".contacts-form");
+const contactsName = contactsPopup.querySelector(".contacts-name");
+const contactsEmail = contactsPopup.querySelector(".contacts-email");
 
-contactsLink.onclick = function () {
-  console.log('Клик по кнопке, отладка');
+let isStorageSupport = true;
+let storage = "";
+
+try {
+  storage = localStorage.getItem("login");
+} catch (err) {
+  isStorageSupport = false;
 }
 
 contactsLink.addEventListener("click", function (evt) {
   evt.preventDefault();
   contactsPopup.classList.add("modal-show");
-    console.log('Добавили класс, отладка');
+
+  if (storage) {
+  contactsName.value = storage;
+  contactsEmail.focus();
+  } else {
     contactsName.focus();
+  }
 });
 
 contactsClose.addEventListener("click", function (evt) {
   evt.preventDefault();
   contactsPopup.classList.remove("modal-show");
+  contactsPopup.classList.remove("modal-error");
+});
+
+contactsForm.addEventListener("submit", function (evt) {
+  if (!contactsName.value || !contactsEmail.value) {
+    evt.preventDefault();
+    contactsPopup.classList.remove("modal-error");
+    contactsPopup.offsetWidth = contactsPopup.offsetWidth;
+    contactsPopup.classList.add("modal-error");
+  }
+  else {
+    if (isStorageSupport) {
+      localStorage.setItem("name", contactsName.value);
+    }
+  }
 });
 
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
     if (contactsPopup.classList.contains("modal-show")) {
       evt.preventDefault();
-      contacts.classList.remove("modal-show");
+      contactsPopup.classList.remove("modal-show");
+      contactsPopup.classList.add("modal-error");
     }
   }
 });
