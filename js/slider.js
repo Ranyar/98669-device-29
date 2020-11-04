506647e31d1e82c57d3f201e52d0b3a0af336160
Ranyar <r4n74r@gmail.com>
2020-11-04 12:37:57 +0500

    Доступная верстка внутренней страницы

--- js/slider.js
+++ js/slider.js
@@ -1,12 +1,20 @@
 const slide1 = document.querySelector(".slide-1");
-const slide2 = document.querySelector(".slide-1");
-const slide3 = document.querySelector(".slide-1");
+const slide2 = document.querySelector(".slide-2");
+const slide3 = document.querySelector(".slide-3");
 
-const button1 = document.querySelector(".slide-1");
-const button2 = document.querySelector(".slide-1");
-const button3 = document.querySelector(".slide-1");
+const button1 = document.querySelector(".slider-button-1");
+const button2 = document.querySelector(".slider-button-2");
+const button3 = document.querySelector(".slider-button-3");
+
 
 button2.onclick = function () {
-  slide1.classList.toggle("slide-hidden");
-  // я ведь в правильную сторону думаю, правда?^^
-}
+ console.log('Отладка');
+};
+
+/* button2.addEventListener("click", function (evt) {
+  evt.preventDefault();
+    console.log('Нажали на кнопку слайдера, отладка');
+      slide1.classList.add("slide-hidden");
+      slide2.classList.toggle("slide-hidden");
+      slide3.classList.add("slide-hidden");
+}); */
